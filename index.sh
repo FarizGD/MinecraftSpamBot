5ba85235bfe77fccf400b75b1e0fbbba66c7889a
FarizGD <165605748+FarizGD@users.noreply.github.com>
2024-12-30 17:07:09 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,2 +1,7 @@
-sudo apt install openjdk-17
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install ca-certificates apt-transport-https gnupg wget
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+sudo apt-get update
+sudo apt-get install -y java-21-amazon-corretto-jdk libxi6 libxtst6 libxrender1
 java -jar mc-bots-1.2.11.jar -s play.louis.phxstore.my.id:19134 -p _ -d 4000 5000 -c 10000 -r
